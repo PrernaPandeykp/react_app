4f1859664dcf5179315e6bf9ad0c3d1122f63535
Prerna_25 <khushipandey2511@gmail.com>
2022-01-31 02:17:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './mycomponents/Header';
+import {Content} from './mycomponents/Content';
+import {Footer} from './mycomponents/Footer';
+import {Offer} from './mycomponents/Offer';
+import {Book} from './mycomponents/Book';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Content/>
+    <Offer/>
+    <Book/>
+    
+    <Footer/>
+    </>
   );
 }
 
